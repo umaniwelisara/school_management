96241eeeb363e996d9b511c4109f966c6f8576be
Ashif Mohamed <ashifm4@gmail.com>
2017-09-20 22:16:22 +0530

    First

---
+++ src/school_management/School_Management.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) Team Extreme. All rights reserved.
+ * Technologies  * 
+ * Language - JAVA  * 
+ * Database - MySQL  * 
+ */
+package school_management;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author wickr
+ */
+public class School_Management {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Connection c = DBConn.myConn();
+        Login_UI login = new Login_UI();
+        login.setVisible(true);
+    }
+    
+}
---
+++ src/school_management/student_management/Student.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) Team Extreme. All rights reserved.
+ * Technologies  * 
+ * Language - JAVA  * 
+ * Database - MySQL  * 
+ */
+package school_management.student_management;
+
+/**
+ *
+ * @author wickr
+ */
+public class Student {
+    
+}
